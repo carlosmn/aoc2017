640b6c1a4fdbdf9262a2fe19bda18d4a42907df3
Carlos Martín Nieto <cmn@dwim.me>
2017-12-08 13:03:30 +0100

    Day 7 part 2

--- src/bin/aoc7.rs
+++ src/bin/aoc7.rs
@@ -26,11 +26,50 @@ fn main() {
     //
     // Or as an alternative, we can go through the list until we find an empty
     // "below".
-    for p in procs.values() {
-        if p.below.borrow().is_none() {
-            println!("The bottom process is {}", p.name);
+    let bottom: &str = (|| {
+        for p in procs.values() {
+            if p.below.borrow().is_none() {
+                return &p.name;
+            }
+        }
+        unreachable!();
+    })();
+
+    println!("The bottom process is {}", bottom);
+
+    // Now we're tasked to find a single process with the wrong weight to be
+    // balanced. We start from the bottom process and see whether all of the
+    // ones above are balanced. If not, we go into the different one and see
+    // whether the ones above it are unbalanced. If they are balanced, we know
+    // it's that base process the one that's unbalanced.
+    let mut pn = bottom;
+    let mut last_common_weight = 0;
+    loop {
+        println!("Looking at {}", pn);
+        println!("Combined weight {}", combined_weight(pn, &procs));
+
+        let above = &procs.get(pn).expect("proc").above;
+        match odd_weight(above, &procs) {
+            Some((odd_one, w)) => {
+                println!("Odd one out {}", odd_one);
+                pn = odd_one;
+                last_common_weight = w;
+                continue;
+            }
+            None => {
+                println!("Above is balanced");
+                break;
+            }
         }
     }
+
+    // last_common_weight is what we need pn's combined weight to be. The
+    // difference between these values is how much we need to change its weight
+    // by.
+    let unbalanced_w = combined_weight(pn, &procs);
+    let d = (last_common_weight as i64) - (unbalanced_w as i64);
+    println!("Unabalanced {}, should weigh {}", pn, procs.get(pn).expect("proc").weight as i64 + d);
+
 }
 
 /// Read the processes from stdin and return them with filled dependencies.
@@ -85,4 +124,49 @@ fn fill_belows(procs: ProcMap) -> ProcMap {
     }
 
     procs
+}
+
+/// Returns the combined weight of the given process together with any above it.
+fn combined_weight(pn: &str, procs: &ProcMap) -> u64 {
+    let p = procs.get(pn).expect("proc");
+    let mut w = p.weight;
+
+    for name in &p.above {
+        w += combined_weight(name, procs);
+    }
+
+    w
+}
+
+/// Returns the name of the process with a different weight than the rest, plus
+/// the common weight. It assumes there is at most a single odd one out and the
+/// rest have the same weight.
+///
+/// If they're all the same, it returns None.
+fn odd_weight<'a>(names: &'a HashSet<String>, procs: &ProcMap) -> Option<(&'a str, u64)> {
+    for name in names {
+        let w = combined_weight(name, procs);
+        let mut common = 0;
+        let mut common_weight = 0;
+
+        for pn in names {
+            if pn == name {
+                continue;
+            }
+
+            let ww = combined_weight(pn, procs);
+            println!("comparing {} ({}) <=> {} ({})", name, w, pn, ww);
+            if ww == w {
+                common += 1;
+            } else {
+                common_weight = ww;
+            }
+        }
+
+        if common == 0 {
+            return Some((name, common_weight));
+        }
+    }
+
+    None
 }
